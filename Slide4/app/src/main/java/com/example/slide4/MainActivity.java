4dab1ac9e3da622d8eb12e08225a1bfdca1f5c3d
uyenlex <21020672@vnu.edu.vn>
2023-09-11 11:28:36 +0700

    add hide soft keyboard and fix UI

--- Slide4/app/src/main/java/com/example/slide4/MainActivity.java
+++ Slide4/app/src/main/java/com/example/slide4/MainActivity.java
@@ -1,12 +1,40 @@
 package com.example.slide4;
 
+import static android.view.inputmethod.EditorInfo.*;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Context;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText nameEditText;
+    private EditText phoneEditText;
+    private Button exitBtn;
+    private Button smsBtn;
+    private RadioGroup option1;
+    private RadioGroup option2;
+    List<CheckBox> items = new ArrayList<CheckBox>();
+    private TextView orderList;
+    private String order1String = "";
+    private String order2String = "";
+    private String otherString = "";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,5 +44,26 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         toolbar.setTitle(R.string.app_name);
         toolbar.setTitleTextColor(Color.WHITE);
+
+        //EditText
+        nameEditText = findViewById(R.id.name_EditText);
+        phoneEditText = findViewById(R.id.phone_EditText);
+        //When finish, close soft keyboard
+        nameEditText.setOnEditorActionListener((v, actionID, event) -> {
+            if (actionID == IME_ACTION_DONE || actionID == IME_ACTION_NEXT) {
+                InputMethodManager i = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                i.hideSoftInputFromWindow(v.getWindowToken(), 0);
+                return true;
+            }
+            return false;
+        });
+        phoneEditText.setOnEditorActionListener((v, actionID, event) -> {
+            if (actionID == IME_ACTION_DONE || actionID == IME_ACTION_NEXT) {
+                InputMethodManager i = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                i.hideSoftInputFromWindow(v.getWindowToken(), 0);
+                return true;
+            }
+            return false;
+        });
     }
 }

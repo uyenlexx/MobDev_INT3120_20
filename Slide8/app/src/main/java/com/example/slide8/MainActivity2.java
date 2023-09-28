9470133b057e670d5317c2ce0f32aa13e13820e2
uyenlex <21020672@vnu.edu.vn>
2023-09-28 11:05:35 +0700

    add dynamic broadcast receiver

--- Slide8/app/src/main/java/com/example/slide8/MainActivity2.java
+++ Slide8/app/src/main/java/com/example/slide8/MainActivity2.java
@@ -1,34 +1,38 @@
 package com.example.slide8;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity2 extends AppCompatActivity {
+    MyBroadcastReceiver receiver = new MyBroadcastReceiver();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
 
-        //broadcast event example
         Intent intent = new Intent("com.example.ACTION_MY_EVENT");
-        intent.putExtra("message", "Hello, this is a static broadcast event");
+        intent.putExtra("message", "Hello, this is a dynamic broadcast event");
 
         Button btn = findViewById(R.id.button);
         btn.setOnClickListener(v -> {
             sendBroadcast(intent);
         });
+    }
 
-        LocalBroadcastManager.getInstance(this).sendBroadcast(intent);
-
-        MyBroadcastReceiver receiver = new MyBroadcastReceiver();
+    @Override
+    protected void onStart() {
+        super.onStart();
         IntentFilter intentFilter = new IntentFilter("com.example.ACTION_MY_EVENT");
-        intentFilter.addAction(getPackageName() + "com.example.ACTION_MY_EVENT");
         registerReceiver(receiver, intentFilter);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        unregisterReceiver(receiver);
     }
 }
